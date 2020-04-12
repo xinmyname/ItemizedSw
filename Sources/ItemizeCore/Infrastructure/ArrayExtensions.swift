0818335c713b2ec64edb64660c251d936db5928e
Andy Sherwood <xinmyname@gmail.com>
2020-04-12 10:12:30 -0700

    Simplify

--- Sources/ItemizeCore/Infrastructure/ArrayExtensions.swift
+++ Sources/ItemizeCore/Infrastructure/ArrayExtensions.swift
@@ -9,63 +9,40 @@
 import Foundation
 
 enum RandomNilStrategy {
-    case always
     case never
-    case fiftyFifty
     case equalChance
-    case probability(value:Float)
 }
 
 extension Array {
     func oneAtRandom(nilStrategy strategy:RandomNilStrategy = .never) -> Element? {
         
         switch strategy {
-        case .always:
-            return nil
+            
         case .never:
             return self[Int(arc4random_uniform(UInt32(self.count)))]
-        case .fiftyFifty:
-            if Int(arc4random_uniform(10)) >= 5 {
-                return self[Int(arc4random_uniform(UInt32(self.count)))]
-            }
-            return nil
+            
         case .equalChance:
             let idx = Int(arc4random_uniform(UInt32(self.count+1)))
             if (idx == self.count) {
                 return nil
             }
             return self[idx]
-        case .probability(let value):
-            if Int(arc4random_uniform(100)) > Int(value * 100.0) {
-                return self[Int(arc4random_uniform(UInt32(self.count)))]
-            }
-            return nil
         }
     }
     
     func oneIndexAtRandom(nilStrategy strategy:RandomNilStrategy = .never) -> Int? {
         
         switch strategy {
-        case .always:
-            return nil
+            
         case .never:
             return Int(arc4random_uniform(UInt32(self.count)))
-        case .fiftyFifty:
-            if Int(arc4random_uniform(10)) >= 5 {
-                return Int(arc4random_uniform(UInt32(self.count)))
-            }
-            return nil
+            
         case .equalChance:
             let idx = Int(arc4random_uniform(UInt32(self.count+1)))
             if (idx == self.count) {
                 return nil
             }
             return idx
-        case .probability(let value):
-            if Int(arc4random_uniform(100)) > Int(value * 100.0) {
-                return Int(arc4random_uniform(UInt32(self.count)))
-            }
-            return nil
         }
     }
 }
